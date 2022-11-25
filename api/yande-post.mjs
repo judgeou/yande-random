0be4581091c08d34a3435d52e9c36501f74b40cc
ouzian <judgeou@vip.qq.com>
2022-11-25 14:34:09 +0800

    add blur mask, add limit, add box shadow

--- api/yande-post.mjs
+++ api/yande-post.mjs
@@ -1,7 +1,7 @@
 import { yande_post } from '../lib/index.mjs'
 
 export default async function handler (req, res) {
-  let { tags } = req.query
-  let posts = await yande_post(tags)
+  let { tags, limit } = req.query
+  let posts = await yande_post(tags, limit)
   res.status(200).json(posts)
 }
